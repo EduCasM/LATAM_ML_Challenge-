62a3f9033d3a418462c26c97bf1c257cf91ae01e
EduCasM <53205307+EduCasM@users.noreply.github.com>
2024-03-22 19:06:02 -0600

    Update api.py
    adding some changes to the api.py file

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,17 @@
 import fastapi
+from challenge.model import DelayModel
+from pydantic import BaseModel
+
+# Please, take a look at the only comment on the 
+# overall section of the challenge.md file.
+
+class Data(BaseModel):
+    OPERA: str
+    TIPOVUELO: Optional[str] = None
+    MES: int
 
 app = fastapi.FastAPI()
+
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
@@ -9,5 +20,18 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(data: Data) -> dict:
+    # creating the instance of the model class
+    model = DelayModel()
+    
+    # Here you need to parse the json Data to 
+    # a pd.DataFrame with the proper format
+    # and the 10 expected features in order
+    # to be able to call :
+    # model.predict(features:  pd.DataFrame)
+    
+    # then we parse put the results on the 
+    # following dictionary:
+    response = {"predict": [0]}
+    
+    return response
